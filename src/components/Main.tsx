379399be3152261f7ff4f171d68c37ada5be8a79
Asirim Ece (asiriece) <asiriece@students.zhaw.ch>
2026-01-06 12:26:26 +0100

    Deploy built site

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
+import Avatar from "../assets/images/avatar.jpeg"
 
 function Main() {
 
@@ -9,7 +10,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="../assets/images/avatar.jpeg" alt="Avatar" />
+          <img src={Avatar} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
